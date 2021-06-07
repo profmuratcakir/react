ca6a05883a4fc38db785e92e45edef59efe2926e
profmuratcakir <mcakiroglu.techproed@gmail.com>
2021-06-07 20:20:39 +0300

    day04

--- ornek8_eventsHooks/src/Events.js
+++ ornek8_eventsHooks/src/Events.js
@@ -1,10 +1,13 @@
 import React from "react";
-
-// * EVENTS (Olaylar) Javascriptteki benzer olarak camelCase ile kullanilirlar.
-// ! Event fonksiyonu yazilirken dikkat edilmelidir. Fonksiyon referans olarak gonderilmelidir. Ornegin:  onClick={handleTikla}
-// * Eger onClick={handleTikla()} seklinde yazilirsa sayfa her render edildiginde dogrudan calismis olur.
-// ? Peki parametreli bir fonksiyon ise ne yapmak gerekir.
-// ? Parametreli fonksiyonlar bir baska isimsiz fonksiyon ile cagrilmalidir.
+//* ====================== OLAYLAR (EVENTS) ============================
+//* - Olaylar, Javascripttekine benzer olarak camelCase ile kullanilirlar.
+//! - Event fonksiyonu yazilirken dikkat edilmelidir. Fonksiyon referans olarak
+//!   gonderilmelidir. Ornegin:  onClick={handleTikla}
+//* - Eger onClick={handleTikla()} seklinde yazilirsa sayfa her render edildiginde
+//*   dogrudan calismis olur.
+//? - Peki parametreli bir fonksiyon ise ne yapmak gerekir.
+//? - Parametreli fonksiyonlar bir baska isimsiz fonksiyon ile cagrilabilir.
+//* =====================================================================
 
 function Events() {
   let baslik = "OLAYLAR";
@@ -15,15 +18,19 @@ function Events() {
   function handleYaz(isim) {
     console.log("Ben " + isim);
   }
-
-  // ? SORU: Degistir Butonuna tiklandiginda baslik yazisini EVENTS olarak degisitirmesini istiyoruz.
-  // ! Degisikligi log'da gorsek de web sayfasında render edilmiyor.
-  // * Bunun sebebi REACT icin bu nesnenin hareketli bir nesne olup olmadigini bilmemesidir.
-  // * React minimum DOM islemi icin default olarak nesneleri guncellemiyor.
-  // ? Guncellenecekleri (interaktif) olanlari biz ona bildirmeliyiz. Bunun için iki yöntem var.
-  // TODO: 1.Yontem Statefull Nesneler kullanmak. Yani state'ler ile degisimi gostermek. (KARMAŞIK)
-  // ! 2. Yontem Hooks adi verilen fonksiyonel yaklasimi kullanmak.
-
+  //?------------------------------------------------------------------
+  //? SORU: Degistir Butonuna tiklandiginda baslik yazisini EVENTS olarak
+  //?       degisitirmesini istiyoruz.
+  //?-------------------------------------------------------------------
+  //! - Degisikligi log'da gorsek de web sayfasında render edilmez.
+  //* - Bunun sebebi REACT icin bu nesnenin hareketli bir nesne olup olmadigini
+  //*   bilmemesidir. React minimum DOM islemi icin default olarak nesneleri
+  //*   guncellemiyor gibi dusunebiliriz.
+  //? - Guncellenecekleri (interaktif) biz ona bildirmeliyiz.
+  //? - Bunun için iki yöntem kullanilmaktadir.
+  //* 1. Statefull Nesneler kullanmak. Yani state'ler ile degisimi gostermek.
+  //* 2. Hooks adi verilen fonksiyonel yaklasimi kullanmak.
+  //?--------------------------------------------------------------------
   function handleDegistir() {
     baslik = "EVENTS";
     console.log(baslik);
